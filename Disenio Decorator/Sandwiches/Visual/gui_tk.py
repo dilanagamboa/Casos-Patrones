af0060e15c5c31cf052fffc06288ee319a5748f7
Marcelo-Matey-R <queridozapato@gmail.com>
2025-11-29 01:44:53 -0600

    Base para la GUI

--- Disenio Decorator/Sandwiches/Visual/gui_tk.py
+++ Disenio Decorator/Sandwiches/Visual/gui_tk.py
@@ -1,1 +1,17 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+
+class SandwichApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Menu de Sandwiches")
+        self.geometry("400x300")
+
+        
+class welcomeScreen(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
+
+        self.label = tk.Label(self, text="Bienvenido a SubGuey", font=("Times New Roman", 16), fg="black")
+        self.label.pack(pady=20)
