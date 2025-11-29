6e8ae1518cc6aeaac5459b041eb26f8dcc27847e
Marcelo-Matey-R <queridozapato@gmail.com>
2025-11-29 17:50:18 -0600

    actualizacion del main

--- Disenio Decorator/Sandwiches/__main__.py
+++ Disenio Decorator/Sandwiches/__main__.py
@@ -1,24 +1,9 @@
-from .Model.decorators import SandwichDecoratorIngredients
-from .Model.ingredients import Ingredients
-from .Services.builder import SandwichBuilder
+from .Visual.gui_tk import SandwichApp
 from .Model.sandwich import *
 
 def main():
-    # Crear vista
-    base = BaseSandwich("", 0.0, SandwichSize.BIG)
-
-    s = base
-
-    s = SandwichBuilder(s).add(Ingredients.BEEF, can=1).add(Ingredients.HONGOS, can=1).add(Ingredients.AGUACATE, can=1).build()
-
-    x = s.ingredient_summary()
-
-    for i in x:
-        print(i)
-    
-    print(s.description(), s.price())
-
-    print(base.size(), type(base.size()))
+    gui = SandwichApp()
+    gui.mainloop()
 
 
 if __name__ == "__main__":
